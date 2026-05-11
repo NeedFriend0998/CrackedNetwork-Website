f55f7dfe6f9058d9ca169e9b325b092dd823a1ba
NeedFriend0998 <bisma2024surabaya@gmail.com>
2026-05-11 22:05:56 +0700

    update JS/main.js

--- JS/main.js
+++ JS/main.js
@@ -851,7 +851,7 @@
         // ========== RENDER STORE ==========
         const storeCategoriesEl = document.getElementById('storeCategories');
         const productsGrid = document.getElementById('productsGrid');
-        let activeCategory = 'bedwars';
+        let activeCategory = 'special';
 
         storeCategories.forEach(cat => {
             const tab = document.createElement('button');
