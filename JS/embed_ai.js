4846c4041c7f4e801de6962900e9081648216a35
NeedFriend0998 <bisma2024surabaya@gmail.com>
2026-08-14 12:51:41 +0700

    js

--- JS/embed_ai.js
+++ JS/embed_ai.js
@@ -1,6 +1,6 @@
 // CrackedNetwork AI Embed
 (function() {
-    const AI_BASE_URL = 'https://37.114.46.114:2306';
+    const AI_BASE_URL = 'http://37.114.46.114:2306';
     const AI_EMBED_ID = 'cn-ai-embed';
     
     function createAIEmbed() {
